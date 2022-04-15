cc1ecfe36c451d02862136c74314860933f83189
roof <roof@odds.team>
2022-04-15 08:13:05 +0700

    Change trait to abstract and introduce Plan

--- src/main/scala/task/Domain.scala
+++ src/main/scala/task/Domain.scala
@@ -1,10 +1,14 @@
 package task
 
-trait Task {
+trait Identifiable {
   val id: Int
-  val detail: String
 }
 
-case class Todo(id: Int, detail: String) extends Task
-case class Doing(id: Int, detail: String) extends Task
-case class Done(id: Int, detail: String) extends Task
+abstract class Task {
+  val detail: String
+}
+case class Plan(detail: String) extends Task
+
+case class Todo(id: Int, detail: String) extends Task with Identifiable
+case class Doing(id: Int, detail: String) extends Task with Identifiable
+case class Done(id: Int, detail: String) extends Task with Identifiable
